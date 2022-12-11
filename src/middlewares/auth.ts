2e3850291a5701004aee3ec1df10ad706bb909a2
Prabeshpd <prabesh1995nepalktm@gmail.com>
2022-12-11 22:46:24 +0545

    feat: add test for middlewares auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,60 +5,62 @@ import { UserDetail } from '../models/user';
 import ErrorFormatter from '../utils/ErrorHandler';
 import { ERROR_TYPES } from '../constants/enums';
 
-interface AuthorizedRequest extends Request {
+export interface AuthorizedRequest extends Request {
   user: UserDetail;
 }
 
 async function authenticate() {
-  return async (request: AuthorizedRequest, response: Response, next: NextFunction) => {
-    try {
-      const { accessTokenSecret } = process.env;
+  return callbackAuthenticate;
+}
 
-      const secret = `${accessTokenSecret}`;
-      const token = request.headers.authorization;
+export async function callbackAuthenticate(request: AuthorizedRequest, response: Response, next: NextFunction) {
+  try {
+    const { accessTokenSecret } = process.env;
 
-      if (!token) {
-        const error = new ErrorFormatter({
-          code: ERROR_TYPES.BAD_REQUEST,
-          message: 'No authorization header set'
-        }).construct();
+    const secret = `${accessTokenSecret}`;
+    const token = request.headers.authorization;
 
-        return next(error);
-      }
-
-      if (!token.includes('Bearer')) {
-        const error = new ErrorFormatter({
-          code: ERROR_TYPES.BAD_REQUEST,
-          message: "Authorization header doesn't include a Bearer token"
-        }).construct();
-
-        return next(error);
-      }
-
-      const bearerToken = token.split(' ')[1];
-
-      try {
-        const decodedResult = (await jwt.verifyToken(bearerToken, secret)) as UserDetail;
-        request.user = decodedResult;
-
-        next();
-      } catch (err) {
-        const error = new ErrorFormatter({
-          code: ERROR_TYPES.UNAUTHORIZED,
-          message: 'INVALID TOKEN'
-        }).construct();
-
-        return next(error);
-      }
-    } catch (err) {
+    if (!token) {
       const error = new ErrorFormatter({
-        code: ERROR_TYPES.INTERNAL_SERVER_ERROR,
-        message: 'Something went wrong'
+        code: ERROR_TYPES.BAD_REQUEST,
+        message: 'No authorization header set'
       }).construct();
 
       return next(error);
     }
-  };
+
+    if (!token.includes('Bearer')) {
+      const error = new ErrorFormatter({
+        code: ERROR_TYPES.BAD_REQUEST,
+        message: "Authorization header doesn't include a Bearer token"
+      }).construct();
+
+      return next(error);
+    }
+
+    const bearerToken = token.split(' ')[1];
+
+    try {
+      const decodedResult = (await jwt.verifyToken(bearerToken, secret)) as UserDetail;
+      request.user = decodedResult;
+
+      next();
+    } catch (err) {
+      const error = new ErrorFormatter({
+        code: ERROR_TYPES.UNAUTHORIZED,
+        message: 'INVALID TOKEN'
+      }).construct();
+
+      return next(error);
+    }
+  } catch (err) {
+    const error = new ErrorFormatter({
+      code: ERROR_TYPES.INTERNAL_SERVER_ERROR,
+      message: 'Something went wrong'
+    }).construct();
+
+    return next(error);
+  }
 }
 
 export default authenticate;
---
+++ test/middlewares/auth.spec.ts
@@ -0,0 +1,128 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { Response, NextFunction } from 'express';
+
+import * as jwt from '../../src/utils/jwt';
+import { UserDetail } from '../../src/models/user';
+import { ERROR_TYPES } from '../../src/constants/enums';
+import { callbackAuthenticate, AuthorizedRequest } from '../../src/middlewares/auth';
+
+describe('resolve config for bootstrapping application', () => {
+  let mockExpressRequest: Partial<AuthorizedRequest>;
+  let mockExpressResponse: Partial<Response>;
+  let mockNextFunction: NextFunction;
+
+  let token: string;
+  let user: UserDetail;
+  before(async () => {
+    mockExpressRequest = {};
+    mockExpressResponse = {};
+    mockNextFunction = () => {};
+    user = {
+      email: 'faker@gmail.com',
+      createdAt: '2022-10-22',
+      isActive: true,
+      updatedAt: '2022-10-22',
+      id: 1
+    };
+    token = await jwt.generateAccessToken(user);
+  });
+
+  it('should return an error if there is no token sent in header', async () => {
+    mockExpressResponse.status = sinon.stub().callsFake((statusArg) => {
+      return {
+        send: sinon.stub().callsFake((replyArg) => {
+          return { status: statusArg, body: replyArg };
+        })
+      };
+    });
+
+    callbackAuthenticate(
+      mockExpressRequest as AuthorizedRequest,
+      mockExpressResponse as Response,
+      mockNextFunction as NextFunction
+    ).then((data: any) =>
+      expect(data).to.deep.equal({
+        status: 400,
+        body: {
+          code: ERROR_TYPES.BAD_REQUEST,
+          msg: 'No authorization header set',
+          details: []
+        }
+      })
+    );
+  });
+
+  it('should throw an error if there is no bearer token sent in header', async () => {
+    mockExpressRequest.headers = { authorization: `random_token ${token}` };
+    mockExpressResponse.status = sinon.stub().callsFake((statusArg) => {
+      return {
+        send: sinon.stub().callsFake((replyArg) => {
+          return { status: statusArg, body: replyArg };
+        })
+      };
+    });
+
+    callbackAuthenticate(
+      mockExpressRequest as AuthorizedRequest,
+      mockExpressResponse as Response,
+      mockNextFunction as NextFunction
+    ).then((data: any) =>
+      expect(data).to.deep.equal({
+        status: 400,
+        body: {
+          code: ERROR_TYPES.BAD_REQUEST,
+          msg: "Authorization header doesn't include a Bearer token",
+          details: []
+        }
+      })
+    );
+  });
+
+  it('should throw an error if the app id on request is different to that of jwt generation app id', async () => {
+    mockExpressRequest.headers = { authorization: `random_token ${token}` };
+    mockExpressResponse.status = sinon.stub().callsFake((statusArg) => {
+      return {
+        send: sinon.stub().callsFake((replyArg) => {
+          return { status: statusArg, body: replyArg };
+        })
+      };
+    });
+
+    callbackAuthenticate(
+      mockExpressRequest as AuthorizedRequest,
+      mockExpressResponse as Response,
+      mockNextFunction as NextFunction
+    ).then((data: any) =>
+      expect(data).to.deep.equal({
+        status: 401,
+        body: {
+          code: ERROR_TYPES.UNAUTHORIZED,
+          msg: 'Invalid Token',
+          details: []
+        }
+      })
+    );
+  });
+
+  it('should set the decoded result in user properties of fastify request object', async () => {
+    mockExpressRequest.headers = { authorization: `Bearer ${token}` };
+    mockExpressResponse.status = sinon.stub().callsFake((statusArg) => {
+      return {
+        send: sinon.stub().callsFake((replyArg) => {
+          return { status: statusArg, body: replyArg };
+        })
+      };
+    });
+
+    process.env.accessTokenSecret = 'ENTER_ACCESS_TOKEN_SALT_HERE';
+
+    callbackAuthenticate(
+      mockExpressRequest as AuthorizedRequest,
+      mockExpressResponse as Response,
+      mockNextFunction as NextFunction
+    ).then((data: any) => {
+      expect(mockExpressRequest.user).to.deep.equal(user);
+    });
+  });
+});
